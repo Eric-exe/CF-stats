153c5adcd71854d235452e10a6dcefc5b2d00890
Eric Lin <ericlin0@meta.com>
2024-07-11 14:29:21 -0700

    remove problems sse

--- server/core/sse.js
+++ server/core/sse.js
@@ -16,19 +16,6 @@ const SSE = {
     removeUserClient(username, client) {
         this.userConnections[username] = this.userConnections[username].filter((conn) => conn !== client);
     },
-
-    problemsConnections: [],
-    sendProblemsUpdate() {
-        for (const client of this.problemsConnections) {
-            client.write("data: " + JSON.stringify({ message: "UPDATED" }) + "\n\n");
-        }
-    },
-    addProblemsClient(client) {
-        this.problemsConnections.push(client);
-    },
-    removeProblemsClient(client) {
-        this.problemsConnection = this.problemsConnection.filter(conn => conn !== client);
-    }
 };
 
 module.exports = SSE;
