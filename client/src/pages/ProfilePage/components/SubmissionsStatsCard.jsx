058b65a1a7471d0fc9ad05572afacad842d345af
Eric Lin <ericlin0@meta.com>
2024-07-16 10:37:37 -0700

    Change submissions to AG charts

--- client/src/pages/ProfilePage/components/SubmissionsStatsCard.jsx
+++ client/src/pages/ProfilePage/components/SubmissionsStatsCard.jsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from "react";
+import { AgGridReact } from "ag-grid-react";
+import ProblemsLinkRenderer from "../../../components/ProblemLinkRenderer";
 import propTypes from "prop-types";
 
 SubmissionsStatsCard.propTypes = {
@@ -5,6 +8,46 @@ SubmissionsStatsCard.propTypes = {
 };
 
 function SubmissionsStatsCard(props) {
+    const [rowData, setRowData] = useState([]);
+
+    const SubmissionLinkRenderer = (params) => {
+        return (
+            <a href={`https://codeforces.com/contest/${params.data.contestId}/submission/${params.data.submissionId}`}>
+                {params.data.submissionId}
+            </a>
+        )
+    }
+
+    const columnDefs = [
+        { field: "submissionId", headerName: "Submission ID", sortable: true, filter: true, cellRenderer: SubmissionLinkRenderer },
+        { field: "name", headerName: "Problem Name", sortable: true, filter: true, cellRenderer: ProblemsLinkRenderer, flex: 1},
+        { field: "verdict", headerName: "Status", sortable: true, filter: true },
+        { field: "time", headerName: "Submission Time" },
+        { field: "programmingLang", headerName: "Programming Language", sortable: true, filter: true },
+        { field: "timeUsed", headername: "Time (ms)", sortable: true, filter: true },
+        { field: "memoryUsed", headerName: "Memory (kb)", sortable: true, filter: true },
+    ];
+
+    useEffect(() => {
+        if (!props.profileInfo.submissions) {
+            return;
+        }
+        const rows = props.profileInfo.submissions.map((submission) => ({
+            // contestId and index field required for link rendering
+            contestId: submission.problem.contestId,
+            index: submission.problem.index,
+
+            submissionId: submission.id,
+            name: submission.problem.name,
+            verdict: submission.verdict,
+            time: new Date(submission.timeCreated).toLocaleString(),
+            programmingLang: submission.programmingLang,
+            timeUsed: submission.timeUsed,
+            memoryUsed: submission.memoryUsed,
+        }));
+        setRowData(rows);
+    }, [props.profileInfo]);
+
     return (
         <div className="card shadow m-4">
             <div className="card-header" data-bs-toggle="collapse" data-bs-target="#submissions-stats-body" role="button">
@@ -12,47 +55,9 @@ function SubmissionsStatsCard(props) {
             </div>
             <div className="collapse show" id="submissions-stats-body">
                 <div className="container-fluid">
-                    <div className="card-body">
-                        <div className="table-responsive" style={{maxHeight: "45vh"}}>
-                            <table className="table table-striped">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Submission ID</th>
-                                        <th scope="col">Problem</th>
-                                        <th scope="col">Status</th>
-                                        <th scope="col">Submission Time</th>
-                                        <th scope="col">Programming Language</th>
-                                        <th scope="col">Time (ms)</th>
-                                        <th scope="col">Memory (kb)</th>
-                                    </tr>
-                                </thead>
-
-                                <tbody className="table-group-divider">
-                                    {props.profileInfo.submissions.slice(0, 1000).map((submission, index) => (
-                                        <tr key={index}>
-                                            <td>
-                                                <a
-                                                    href={`https://codeforces.com/contest/${submission.problem.contestId}/submission/${submission.id}`}
-                                                >
-                                                    {submission.id}
-                                                </a>
-                                            </td>
-                                            <td>
-                                                <a
-                                                    href={`https://codeforces.com/contest/${submission.problem.contestId}/problem/${submission.problem.index}`}
-                                                >
-                                                    {submission.problem.name}
-                                                </a>
-                                            </td>
-                                            <td>{submission.verdict}</td>
-                                            <td>{new Date(submission.timeCreated).toLocaleString()}</td>
-                                            <td>{submission.programmingLang}</td>
-                                            <td>{submission.timeUsed}</td>
-                                            <td>{submission.memoryUsed / 1000}</td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
+                    <div className="card-body d-flex justify-content-center">
+                        <div className="ag-theme-alpine" style={{ height: "45vh", width: "95vw" }}>
+                            <AgGridReact columnDefs={columnDefs} rowData={rowData} />
                         </div>
                     </div>
                 </div>
