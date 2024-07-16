856b47ebb213abf021e605acc92c647fdff1b59f
Eric Lin <ericlin0@meta.com>
2024-07-16 10:07:37 -0700

    Bug fix where rows are only painted sometimes

--- client/src/pages/ProblemsPage/ProblemsPage.jsx
+++ client/src/pages/ProblemsPage/ProblemsPage.jsx
@@ -10,6 +10,7 @@ ProblemsPage.propTypes = {
 };
 
 function ProblemsPage(props) {
+    const [problemsData, setProblemsData] = useState([]);
     const [rowData, setRowData] = useState([]);
 
     const filterParams = {
@@ -26,49 +27,47 @@ function ProblemsPage(props) {
 
     useEffect(() => {
         const getProblemsData = async () => {
-            const problemsData = await API.getProblemsData().then((response) => response.json());
-            // turn into hashmap for faster status updates for the user
-            const data = problemsData.map((problem) => ({
-                id: problem.id,
-                contestId: problem.contestId,
-                index: problem.index,
-                name: problem.name,
-                rating: problem.rating === -1 ? null : problem.rating,
-                tags: problem.tags,
-                status: "None",
-            }));
-            setRowData(data);
+            const data = await API.getProblemsData().then((response) => response.json());
+            // because the db isn't guaranteed to be sorted, sort the info
+            data.sort((problemA, problemB) => problemB.contestId - problemA.contestId);
+            setProblemsData(data);
         };
         getProblemsData();
     }, []);
 
     // paint the rows different color if the user has solved/attempted a problem
     useEffect(() => {
-        if (!props.userInfo || !props.userInfo.problemStatuses) return;
-        const updatedRowData = [...rowData];
-        for (const problemStatus of props.userInfo.problemStatuses) {
-            const index = updatedRowData.findIndex((row) => row.id === problemStatus.problem.id);
-            if (index !== -1) {
-                if (problemStatus.AC === 0) {
-                    updatedRowData[index].status = "FAILED";
-                } else {
-                    updatedRowData[index].status = "OK";
-                }
+        const rows = problemsData.map((problem) => {
+            const problemStatus = props.userInfo.problemStatuses
+                ? props.userInfo.problemStatuses.find((status) => status.problem.id === problem.id)
+                : undefined;
+            
+            let status = problemStatus
+                ? (problemStatus.AC > 0 ? "OK" : "FAILED")
+                : "None";
+            
+            return {
+                id: problem.id,
+                contestId: problem.contestId,
+                index: problem.index,
+                name: problem.name,
+                rating: problem.rating === -1 ? null : problem.rating,
+                tags: problem.tags,
+                status
             }
-        }
-        setRowData(updatedRowData);
-    }, [props.userInfo]);
+        });
+        setRowData(rows);
+    }, [props.userInfo, problemsData]);
 
     const gridOptions = {
-        getRowStyle: params => {
+        getRowStyle: (params) => {
             if (params.data.status === "OK") {
-                return { background: "lightgreen" }
+                return { background: "lightgreen" };
+            } else if (params.data.status === "FAILED") {
+                return { background: "lightpink" };
             }
-            else if (params.data.status === "FAILED") {
-                return { background: "salmon" }
-            }
-        }
-    }
+        },
+    };
 
     return (
         <div className="card shadow m-4">
